9c463399c014cc6e6d25ca40e5ef8b1130a06627
MatiasGSB <97728217+MatiasGSB@users.noreply.github.com>
2022-01-14 09:55:39 +0100

    Add files via upload

---
+++ MainEx.java
@@ -0,0 +1,7 @@
+public class MainEx {
+    public static void main(String[] args) {
+        System.out.println("Demo SCV");
+        System.out.println("We start working in a new version!");
+    }
+}
+
